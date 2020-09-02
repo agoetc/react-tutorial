ddd4a67b3126d79adbae318c23233dc5b416b8a9
Shogo Takegawa <b6151@oic.jp>
2020-09-02 23:57:03 +0900

    インタラクティブ

--- src/main/scala/hello/world/component/Square.scala
+++ src/main/scala/hello/world/component/Square.scala
@@ -1,17 +1,24 @@
 package hello.world.component
 
-import slinky.core.StatelessComponent
+import slinky.core.Component
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
 import slinky.web.html._
 
+@react class Square extends Component {
 
-@react class Square extends StatelessComponent {
+  case class State(value: Option[String])
+
+  def initialState: State = State(None)
+
   case class Props(index: Int)
 
   def render(): ReactElement = {
-    button(className := "square")(
-      props.index
+    button(
+      className := "square",
+      onClick := (_ => setState(State(Some("X"))))
+    )(
+      state.value
     )
   }
 }
