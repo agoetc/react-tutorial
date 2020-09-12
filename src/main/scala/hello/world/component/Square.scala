1a8171c8ee45e2fa57c9f817582f2a7095dc1fd0
Shogo Takegawa <b6151@oic.jp>
2020-09-12 18:46:12 +0900

    Squareを関数コンポーネントに

--- src/main/scala/hello/world/component/Square.scala
+++ src/main/scala/hello/world/component/Square.scala
@@ -1,19 +1,15 @@
 package hello.world.component
 
-import slinky.core.Component
+import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
-import slinky.core.facade.ReactElement
 import slinky.web.html._
 
-@react class Square extends Component {
+@react object Square {
 
-  case class State(value: Option[String])
-
-  def initialState: State = State(None)
 
   case class Props(value: Option[String], onClick: () => Unit)
 
-  def render(): ReactElement = {
+  val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
     button(
       className := "square",
       onClick := props.onClick
